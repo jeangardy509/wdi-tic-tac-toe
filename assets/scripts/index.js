6be4716717ff028919a54e38d487c38d783e5b4e
jeangardy509 <jeangardy509@gmail.com>
2018-02-12 09:42:51 -0500

    Made some few changes to the index folder

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,6 +2,7 @@
 
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
+const authEvents = require('./auth/events.js')
 
 $(() => {
   setAPIOrigin(location, config)
@@ -12,3 +13,10 @@ $(() => {
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
+
+$(() => {
+  authEvents.addHandlers()
+
+  // $('#sign-out').on('submit', authEvents.onSignOut)
+  // $('#sign-up').on('submit', authEvents.onSignUp)
+})
