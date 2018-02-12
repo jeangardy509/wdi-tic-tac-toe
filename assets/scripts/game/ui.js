4613c346992234c273a7b946a6facb37606e1bf7
jeangardy509 <jeangardy509@gmail.com>
2018-02-12 09:48:58 -0500

    Haven't created any codes in this folder yet!

---
+++ assets/scripts/game/ui.js
@@ -0,0 +1,19 @@
+'use strict'
+
+// const onSignUpSuccess = function (data) {
+//   $('#message').text('Example successfully created')
+//   $('#message').css('background-color, green')
+//   console.log('successfully created an example')
+//   console.log(data)
+// }
+//
+// const onSignUpFailure = function (error) {
+//   $('#message').text('Erro on create of example')
+//   $('#message').css('background-color, red')
+//   console.log(error)
+// }
+//
+// module.exports = {
+//   onSignUpSuccess,
+//   onSignUpFailure
+// }
